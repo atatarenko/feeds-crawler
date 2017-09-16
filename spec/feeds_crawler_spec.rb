8c892cd3935bb077cbb4baad31afc2f3cc912a5a
Andrey Tatarenko <andrey17076@gmail.com>
2017-09-16 22:21:20 +0300

    Add integration test

--- spec/feeds_crawler_spec.rb
+++ spec/feeds_crawler_spec.rb
@@ -2,20 +2,21 @@ require 'spec_helper'
 
 RSpec.describe FeedsCrawler do
   describe '.crawl' do
-    let(:article) { 'article' }
+    let(:rss_url) { 'spec/fixtures/rss_feed.xml' }
 
-    before do
-      allow_any_instance_of(Crawlers::Rss).to receive(:articles).and_return([article])
-    end
-
-    subject { described_class.crawl('url') }
+    subject { described_class.crawl(rss_url) }
 
     it 'crawls crawlers feeds articles' do
-      is_expected.to match_array([article])
+      is_expected.to match_array(['Real Content'])
     end
 
     context 'when passing an argument list' do
+      let(:article) { 'article' }
       let(:rss_urls) { %w[first_url second_url] }
+
+      before do
+        allow_any_instance_of(Crawlers::Rss).to receive(:articles).and_return([article])
+      end
 
       subject { described_class.crawl(*rss_urls).count }
 
